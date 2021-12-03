2b7f2dc05f3a2f83d88a801d60416cc9c916f5ee
Коулинз Эфимера <never.alive.king@yandex.ru>
2021-12-03 23:36:13 +0300

    1.1.1.1 ver 2 for exercize

--- TestProjectAddressbookNumberTwo/GroupCreationTest.cs
+++ TestProjectAddressbookNumberTwo/GroupCreationTest.cs
@@ -7,10 +7,10 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
-namespace TestProjectAddressbookNumberTwo
+namespace webAddressBookTests
 {
     [TestFixture]
-    public class UntitledTestCase
+    public class GroupCreationTest
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
@@ -40,7 +40,7 @@ namespace TestProjectAddressbookNumberTwo
         }
 
         [Test]
-        public void TheUntitledTestCaseTest()
+        public void GroupCreationTests()
         {
             driver.Navigate().GoToUrl(baseURL);
             driver.FindElement(By.Name("user")).Click();
