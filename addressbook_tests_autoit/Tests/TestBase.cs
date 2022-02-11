9c1d1bdc7fef403475cea78561dcb2d4b5131a34
Коулинз Эфимера <never.alive.king@yandex.ru>
2022-02-11 14:28:28 +0300

    18 упражнение

---
+++ addressbook_tests_autoit/Tests/TestBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    public class TestBase
+    {
+        public AppManager app;
+
+        [SetUp]
+        public void initApplication()
+        {
+            app = new AppManager();
+        }
+        [TearDown]
+        public void stopApplication()
+        {
+            app.Stop();
+        }
+    }
+}
